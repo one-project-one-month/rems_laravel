5b68fee8f77816620001629b5bbd1e3ca5a76ac3
Pyae Phyo Khant <146649651+pyaephyokhant760@users.noreply.github.com>
2024-07-24 16:51:15 +0630

    api_crud_and_migration_update_by_PPK

---
+++ app/Http/Controllers/api/transactionController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+
+class transactionController extends Controller
+{
+    //create
+    public function create(Request $request) {
+        $validator = $this->validator($request);
+        if($validator->fails()){
+            return response()->json(['status' => false,'message'=> 'validation error','errors' => $validator->errors()],200);
+        }
+        $data = $this->Data($request);
+        Transaction::create($data);
+        return response()->json(['status' => true,'message' => $data],200);
+
+    }
+
+    // view
+    public function view() {
+        $data = Transaction::get();
+        return response()->json($data, 200);
+    }
+
+    public function view2($id) {
+        $data = Transaction::where('id',$id)->first();
+        return response()->json($data, 200);
+    }
+
+    // updata
+    public function update(Request $request) {
+
+        $transaction = Transaction::where('id',$request->id)->first();
+
+
+
+        if (isset($transaction)) {
+            $data = $this->Dataupdate($request);
+            Transaction::where('id',$request->id)->update($data);
+            return response()->json(["Status" => true,"Message" => "Update Success"], 200);
+        }
+        return response()->json(["Status" => false,"Message" => "Try Again"], 200);
+    }
+
+    // delete
+    public function delete(Request $request) {
+        $data = Transaction::where('id',$request->id)->first();
+        if (isset($data)) {
+            Transaction::where('id',$request->id)->delete();
+            return response()->json(["Status" => true,"Message" => "Delete Success"], 200);
+        }
+        return response()->json(["Status" => false,"Message" => "Have's Id"], 200);
+    }
+
+
+
+    // validator
+    private function validator($request) {
+        $validator = Validator::make($request->all(), [
+            'property_id' => 'required',
+            'buyer_id' => 'required',
+            'agent_id' => 'required',
+            'sale_price' => 'required',
+            'commission' => 'required',
+            'status' => 'required'
+        ]);
+        return $validator;
+    }
+
+    // Data
+    private function Data($request) {
+        return [
+            'property_id' => $request->property_id,
+            'buyer_id' => $request->buyer_id,
+            'agent_id' => $request->agent_id,
+            'transaction_date' => $request->transaction_date,
+            'sale_price' => $request->sale_price,
+            'commission' => $request->commission,
+            'status' => $request->status
+        ];
+    }
+
+    // Dataupdate
+    private function Dataupdate($request) {
+        return [
+            'id' => $request->id,
+            'property_id' => $request->property_id,
+            'buyer_id' => $request->buyer_id,
+            'agent_id' => $request->agent_id,
+            'transaction_date' => $request->transaction_date,
+            'sale_price' => $request->sale_price,
+            'commission' => $request->commission,
+            'status' => $request->status
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,14 @@
 <?php
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Controller;
-
-use App\Http\Controllers\api\UserController;
-use App\Http\Controllers\api\ClientController;
-use App\Http\Controllers\api\AgentController;
-use App\Models\Agent;
 use App\Models\User;
+use App\Models\Agent;
+use Illuminate\Http\Request;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\UserController;
+use App\Http\Controllers\api\AgentController;
+use App\Http\Controllers\api\ClientController;
+use App\Http\Controllers\api\transactionController;
 
 
 
@@ -17,3 +18,21 @@ Route::post("logout",[UserController::class,"logout"])->middleware('auth:sanctum
 Route::apiResource('users',UserController::class);
  Route::apiResource("clients",ClientController::class);
  Route::apiResource("agents",AgentController::class);
+
+
+ Route::post('transaction/create',[transactionController::class,'create']);
+ Route::get('transaction/view',[transactionController::class,'view']);
+ Route::get('transaction/view/{id}',[transactionController::class,'view2']);
+ Route::post('transaction/delete',[transactionController::class,'delete']);
+ Route::post('transaction/update',[transactionController::class,'update']);
+
+
+
+
+
+
+
+//  http://127.0.0.1:8000/api/transaction/create
+//  http://127.0.0.1:8000/api/transaction/view
+//  http://127.0.0.1:8000/api/transaction/update
+//  http://127.0.0.1:8000/api/transaction/delete
