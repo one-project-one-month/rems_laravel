71e612e391acb6a1c6a68b18ff5441472563b802
Thinn Thuzar <thinnthuzar89@gmail.com>
2024-07-25 23:50:22 +0630

    ttzar-updateregister-openapi-collections

--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,11 @@ use App\Models\User;
 
 
 
-Route::post("register",[UserController::class,"register"]);
+
 Route::post("login",[UserController::class,"login"]);
+Route::post("register",[UserController::class,"register"]);
+Route::post("registerAgent",[UserController::class,"registerAgent"]);
+Route::post("registerClient",[UserController::class,"registerClient"]);
 Route::post("logout",[UserController::class,"logout"])->middleware('auth:sanctum');
 Route::apiResource('users',UserController::class);
  Route::apiResource("clients",ClientController::class);
