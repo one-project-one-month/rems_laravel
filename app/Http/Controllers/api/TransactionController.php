03ed53daac3669a11111877ecdc73e122099da48
Pyae Phyo Khant <146649651+pyaephyokhant760@users.noreply.github.com>
2024-08-07 16:05:59 +0630

    paginate_and_update_delete_BY_Transaction

--- app/Http/Controllers/api/TransactionController.php
+++ app/Http/Controllers/api/TransactionController.php
@@ -19,7 +19,7 @@ class TransactionController extends Controller
      */
     public function index()
     {
-        $data = Transaction::all();
+        $data = Transaction::paginate(5);
         return ['data' => $data];
     }
 
@@ -54,39 +54,4 @@ class TransactionController extends Controller
         return response()->json(['status' => False ,'message' => 'Try Again'],200);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        $data = $request->validate([
-            'property_id' => 'required',
-            'client_id' => 'required',
-            'rental_period' => 'required',
-            'transaction_date' => 'required',
-            'sale_price' => 'required',
-            'commission' => 'required',
-            'status' => 'required'
-         ]);
-
-         $searchId = Transaction::where('id',$id)->first();
-         if(isset($searchId)) {
-             $update = Transaction::where('id',$id)->update($data);
-             return response()->json(['message' => 'Update Success'], 200);
-         }
-         return response()->json(['status' => False ,'message' => 'Try Again'],200);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        $searchId = Transaction::where('id',$id)->first();
-        if(isset($searchId)) {
-            $data = Transaction::where('id',$id)->delete();
-            return response()->json(['message' => True,], 200);
-        }
-        return response()->json(['status' => False ,'message' => 'Try Again'],200);
-    }
 }
